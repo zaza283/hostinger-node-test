48a84166126cb2d1c70e53c917c9e30919dcf295
‪Mhmoud AL-zaza‬‏ <103927409+zaza283@users.noreply.github.com>
2026-02-10 11:56:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.send("OK - Hostinger Node App"));
+
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("✅ Express running on Hostinger"));
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server listening on", PORT);
-});
+app.listen(PORT, "0.0.0.0", () => console.log("Running on port", PORT));
