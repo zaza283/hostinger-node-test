7a324b4badc9df3b0dec8525308afd36617c7fed
‪Mhmoud AL-zaza‬‏ <103927409+zaza283@users.noreply.github.com>
2026-02-10 11:35:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("✅ Node.js is running on Hostinger");
+  res.send("OK - Hostinger Node is running ✅");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server started on port", PORT);
+  console.log("Server running on port", PORT);
 });
